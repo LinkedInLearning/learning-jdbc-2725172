cde01561a9562698c7f415187177810e128e373d
Luigi Mota <luigi_mota89@hotmail.com>
2025-02-21 04:22:29 +0000

    set up the create method of the service dao, note the try within the try for the connection and statement objects

--- my-app/src/main/java/com/loogibot/lil/data/dao/ServiceDao.java
+++ my-app/src/main/java/com/loogibot/lil/data/dao/ServiceDao.java
@@ -16,19 +16,19 @@ import javax.xml.crypto.Data;
 import com.loogibot.lil.data.entity.Service;
 import com.loogibot.lil.data.util.DatabaseUtils;
 
-public class ServiceDao implements Dao<Service, UUID>  {
+public class ServiceDao implements Dao<Service, UUID> {
   private static final Logger LOGGER = Logger.getLogger(ServiceDao.class.getName());
   private static final String GET_ALL = "select service_id, name, price from wisdom.services";
   private static final String GET_BY_ID = "select service_id, name, price from wisdom.services where service_id = ?";
-  
+  private static final String CREATE = "insert into wisdom.services (service_id, name, price) values (?,?,?)";
 
   @Override
   public List<Service> getAll() {
     // TODO Auto-generated method stub
     List<Service> services = new ArrayList<>();
     Connection connection = DatabaseUtils.getConnection();
-    
-    try(Statement statement = connection.createStatement()){
+
+    try (Statement statement = connection.createStatement()) {
       // make sure its not beans.Statement, use sql.Statement
       ResultSet rs = statement.executeQuery(GET_ALL);
       services = this.processResultSet(rs);
@@ -42,17 +42,40 @@ public class ServiceDao implements Dao<Service, UUID>  {
   @Override
   public Service create(Service entity) {
     // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'create'");
+    UUID serviceId = UUID.randomUUID();
+    Connection connection = DatabaseUtils.getConnection();
+    try {
+      connection.setAutoCommit(false);
+      PreparedStatement statement = connection.prepareStatement(CREATE);
+      statement.setObject(1, serviceId);
+      statement.setString(2, entity.getName());
+      statement.setBigDecimal(3, entity.getPrice());
+      statement.execute();
+      connection.commit();
+      statement.close();
+    } catch (SQLException e) {
+      try{
+        connection.rollback();
+      } catch (SQLException sqle) {
+        DatabaseUtils.handleSqlException("ServiceDao.create.rollback", sqle, LOGGER);
+      }
+      DatabaseUtils.handleSqlException("ServiceDao.create", e, LOGGER);
+    }
+    Optional<Service> service = this.getOne(serviceId);
+    if (!service.isPresent()){
+      return null;
+    }
+    return service.get();
   }
 
   @Override
   public Optional<Service> getOne(UUID id) {
     // TODO Auto-generated method stub
-    try(PreparedStatement statement = DatabaseUtils.getConnection().prepareStatement(GET_BY_ID)){
+    try (PreparedStatement statement = DatabaseUtils.getConnection().prepareStatement(GET_BY_ID)) {
       statement.setObject(1, id);
       ResultSet rs = statement.executeQuery();
       List<Service> services = this.processResultSet(rs);
-      if(services.isEmpty()){
+      if (services.isEmpty()) {
         return Optional.empty();
       }
       return Optional.of(services.get(0));
@@ -75,11 +98,11 @@ public class ServiceDao implements Dao<Service, UUID>  {
     throw new UnsupportedOperationException("Unimplemented method 'delete'");
   }
 
-  private List<Service> processResultSet(ResultSet rs) throws SQLException{
+  private List<Service> processResultSet(ResultSet rs) throws SQLException {
     List<Service> services = new ArrayList<>();
-    while(rs.next()){
+    while (rs.next()) {
       Service service = new Service();
-      service.setServiceId((UUID)rs.getObject("service_id"));
+      service.setServiceId((UUID) rs.getObject("service_id"));
       service.setName(rs.getString("name"));
       service.setPrice(rs.getBigDecimal("price"));
       services.add(service);
