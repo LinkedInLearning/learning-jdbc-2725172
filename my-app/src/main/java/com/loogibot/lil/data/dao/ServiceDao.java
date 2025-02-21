c7e56d4dc2889de3f7ce642bdb110c8b9b42b4e5
Luigi Mota <luigi_mota89@hotmail.com>
2025-02-21 15:54:39 +0000

    delete method added, though I'll need to try this again. I notice with this project, I don't know enough of the connection, service and postgresql functions to fully understand how its working, notably their respective methods and where/how to use them, unlike Android Room DB I am much more familiar with the DAO system

--- my-app/src/main/java/com/loogibot/lil/data/dao/ServiceDao.java
+++ my-app/src/main/java/com/loogibot/lil/data/dao/ServiceDao.java
@@ -22,6 +22,7 @@ public class ServiceDao implements Dao<Service, UUID> {
   private static final String GET_BY_ID = "select service_id, name, price from wisdom.services where service_id = ?";
   private static final String CREATE = "insert into wisdom.services (service_id, name, price) values (?,?,?)";
   private static final String UPDATE = "update wisdom.services set name = ?, price = ? where service_id = ?";
+  private static final String DELETE = "delete from wisdom.services where service_id = ?";
 
   @Override
   public List<Service> getAll() {
@@ -55,7 +56,7 @@ public class ServiceDao implements Dao<Service, UUID> {
       connection.commit();
       statement.close();
     } catch (SQLException e) {
-      try{
+      try {
         connection.rollback();
       } catch (SQLException sqle) {
         DatabaseUtils.handleSqlException("ServiceDao.create.rollback", sqle, LOGGER);
@@ -63,7 +64,7 @@ public class ServiceDao implements Dao<Service, UUID> {
       DatabaseUtils.handleSqlException("ServiceDao.create", e, LOGGER);
     }
     Optional<Service> service = this.getOne(serviceId);
-    if (!service.isPresent()){
+    if (!service.isPresent()) {
       return null;
     }
     return service.get();
@@ -115,7 +116,23 @@ public class ServiceDao implements Dao<Service, UUID> {
   @Override
   public void delete(UUID id) {
     // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    Connection connection = DatabaseUtils.getConnection();
+
+    try {
+      connection.setAutoCommit(false);
+      PreparedStatement statement = connection.prepareStatement(DELETE);
+      statement.setObject(1, id);
+      statement.execute();
+      connection.commit();
+      statement.close();
+    } catch (SQLException e) {
+      try {
+        connection.rollback();
+      } catch (SQLException sqle) {
+        DatabaseUtils.handleSqlException("ServiceDao.delete.rollback", sqle, LOGGER);
+      }
+      DatabaseUtils.handleSqlException("ServiceDao.delete", e, LOGGER);
+    }
   }
 
   private List<Service> processResultSet(ResultSet rs) throws SQLException {
