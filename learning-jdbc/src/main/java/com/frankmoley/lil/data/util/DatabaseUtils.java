fa21b667a2ad75e9ee07bfb8226ee5d07ec86b5f
contactjs078 <contactjs078@gmail.com>
2025-08-13 12:36:43 -0400

    update postgres USERNAME PASSWORD

--- learning-jdbc/src/main/java/com/frankmoley/lil/data/util/DatabaseUtils.java
+++ learning-jdbc/src/main/java/com/frankmoley/lil/data/util/DatabaseUtils.java
@@ -7,19 +7,19 @@ import java.util.logging.Logger;
 
 public class DatabaseUtils {
   private static final String URL = "jdbc:postgresql://localhost:5432/localdb";
-  private static final String USERNAME = "localdbuser";
-  private static final String PASSWORD = "P@ssw0rd!";
+  private static final String USERNAME = "postgres";
+  private static final String PASSWORD = "admin";
   private static final Logger LOGGER = Logger.getLogger(DatabaseUtils.class.getName());
   private static final String exceptionFormat = "exception in %s, message: %s, code: %s";
   private static Connection connection;
-  
-  public static Connection getConnection(){
-    if(connection == null){
-      synchronized(DatabaseUtils.class){
-        if(connection == null){
-          try{
+
+  public static Connection getConnection() {
+    if (connection == null) {
+      synchronized (DatabaseUtils.class) {
+        if (connection == null) {
+          try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-          }catch(SQLException e){
+          } catch (SQLException e) {
             handleSqlException("DatabaseUtils.getConnection", e, LOGGER);
           }
         }
@@ -28,7 +28,7 @@ public class DatabaseUtils {
     return connection;
   }
 
-  public static void handleSqlException(String method, SQLException e, Logger log){
+  public static void handleSqlException(String method, SQLException e, Logger log) {
     log.warning(String.format(exceptionFormat, method, e.getMessage(), e.getErrorCode()));
     throw new RuntimeException(e);
   }
